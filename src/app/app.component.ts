a783f88ec51f700bd5904e5dfcdf75b7c5815e3c
Ferley Esteban Arias Pulido <ferley.arias@marketmix.com.co>
2023-09-07 21:38:40 -0500

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-store';
+  http = inject(HttpClient)
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = 'changed';
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+      })
+  }
 }
